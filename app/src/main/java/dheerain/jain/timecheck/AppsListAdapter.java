f123e833d6b54d956c0a6226b7aa3a21a1abbc7e
dheerain-jain <jain.dheerain@gmail.com>
2017-08-27 13:44:21 +0530

    AppStatsWorking;AggregateAdded

--- app/src/main/java/dheerain/jain/timecheck/AppsListAdapter.java
+++ app/src/main/java/dheerain/jain/timecheck/AppsListAdapter.java
@@ -1,32 +1,67 @@
 package dheerain.jain.timecheck;
 
+import android.app.usage.UsageStats;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by Dheerain on 27-08-2017.
  */
 
 public class AppsListAdapter extends RecyclerView.Adapter<AppsListAdapter.ViewHolder> {
-    @Override
-    public AppsListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+
+    Context context;
+    List<UsageStats> usageStatses;
+    public AppsListAdapter(Context c, List<UsageStats> usageStatses) {
+
+        context=c;
+        this.usageStatses=usageStatses;
+
     }
 
     @Override
+    public AppsListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.layout_apps,parent,false);
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
     public void onBindViewHolder(AppsListAdapter.ViewHolder holder, int position) {
+
+        holder.name.setText(usageStatses.get(position).getPackageName());
+        try {
+            holder.icon.setImageDrawable(context.getPackageManager()
+                    .getApplicationIcon(usageStatses.get(position).getPackageName()));
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return usageStatses.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+        ImageView icon;
         public ViewHolder(View itemView) {
             super(itemView);
+            name= (TextView) itemView.findViewById(R.id.name);
+            icon= (ImageView) itemView.findViewById(R.id.appIcon);
         }
     }
 }
--- app/src/main/java/dheerain/jain/timecheck/MainActivity.java
+++ app/src/main/java/dheerain/jain/timecheck/MainActivity.java
@@ -1,12 +1,22 @@
 package dheerain.jain.timecheck;
 
 import android.app.AppOpsManager;
+import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
 
 import static android.app.AppOpsManager.MODE_ALLOWED;
 import static android.app.AppOpsManager.OPSTR_GET_USAGE_STATS;
@@ -15,7 +25,9 @@ import static android.os.Process.myUid;
 public class MainActivity extends AppCompatActivity {
 
     UsageStatsManager usageStatsManager;
+    RecyclerView recyclerView;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +37,22 @@ public class MainActivity extends AppCompatActivity {
         }else{
             usageStatsManager = (UsageStatsManager) getApplicationContext().getSystemService(Context.USAGE_STATS_SERVICE);
         }
+        recyclerView= (RecyclerView) findViewById(R.id.ReyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MONTH, -1);
+        long start = calendar.getTimeInMillis();
+        long end = System.currentTimeMillis();
+/*
+        List<UsageStats> stats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_WEEKLY, start, end);
+*/
+
+        Map<String, UsageStats> usageStats = usageStatsManager.queryAndAggregateUsageStats(start,end);
+        List<UsageStats> stats = new ArrayList<>();
+        stats.addAll(usageStats.values());
+        recyclerView.setAdapter(new AppsListAdapter(this,stats));
+
     }
 
 
